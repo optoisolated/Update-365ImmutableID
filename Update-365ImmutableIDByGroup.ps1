1b25c35b578605173cb7169f82200e8af18e133b
Optoisolated <46586989+optoisolated@users.noreply.github.com>
2019-05-30 16:59:07 +1000

    Update Update-365ImmutableIDByGroup.ps1

--- Update-365ImmutableIDByGroup.ps1
+++ Update-365ImmutableIDByGroup.ps1
@@ -17,9 +17,23 @@ ForEach ($User in $ADGroup) {
   [GUID]$ADGUID = (Get-ADUser $ADUser).ObjectGUID
 
   If ($ADGUID -ne $null) {
+      #Generate the ImmutableID
       $ImmutableID = [System.Convert]::ToBase64String($ADGUID.ToByteArray())
       Write-Host "Updating Immutable ID for O365 User '$365User' with the Immutable ID '$ImmutableID'"
+      
+      #Update the Immutable ID in O365
       Set-MsolUser -UserPrincipalName "$365User" -ImmutableID $ImmutableID
+      
+      # Wait a few seconds to wait for things to update in the cloud
+      Start-Sleep -s 2
+      
+      #Check that the Immutable ID updated successfully.
+      Get-MsolUser -UserPrincipalName $365User | Select ImmutableID
+      if ($IID.ImmutableID -eq $ImmutableID) {
+        Write-Host "O365 User '$365User' Immutable ID Updated Successfully."
+      } ELSE { 
+        Write-Host "O365 User '$365User' Immutable ID Update Failed!"
+      }
   } Else {
       Write-Host "Error generating Immutable ID. Office365 has not been updated."
   }
